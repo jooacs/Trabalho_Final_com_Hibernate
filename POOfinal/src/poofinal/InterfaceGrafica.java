1944e39f78d4d5a57829d10e3144ff776ca1fec8
jooacs <johnnyaguido@outlook.com>
2025-05-17 01:05:49 -0300

    Teste de mudança de painel, painel 1 para o 2 em apenas um Frame na própria IDE do netbeans

--- POOfinal/src/poofinal/InterfaceGrafica.java
+++ POOfinal/src/poofinal/InterfaceGrafica.java
@@ -4,6 +4,8 @@
  */
 package poofinal;
 
+import java.awt.CardLayout;
+
 /**
  *
  * @author johnn
@@ -26,40 +28,88 @@ public class InterfaceGrafica extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1_Adicionar_Aluno = new javax.swing.JButton();
+        PPainel = new javax.swing.JPanel();
+        Adicionar_Alunos = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        Ver_dados = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1_Adicionar_Aluno.setText("Adicionar Aluno");
-        jButton1_Adicionar_Aluno.addActionListener(new java.awt.event.ActionListener() {
+        PPainel.setLayout(new java.awt.CardLayout());
+
+        jLabel1.setText("Painel 1");
+
+        jButton1.setText("Ir para 2");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1_Adicionar_AlunoActionPerformed(evt);
+                jButton1ActionPerformed(evt);
             }
         });
+
+        javax.swing.GroupLayout Adicionar_AlunosLayout = new javax.swing.GroupLayout(Adicionar_Alunos);
+        Adicionar_Alunos.setLayout(Adicionar_AlunosLayout);
+        Adicionar_AlunosLayout.setHorizontalGroup(
+            Adicionar_AlunosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Adicionar_AlunosLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(Adicionar_AlunosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(jButton1))
+                .addContainerGap(516, Short.MAX_VALUE))
+        );
+        Adicionar_AlunosLayout.setVerticalGroup(
+            Adicionar_AlunosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Adicionar_AlunosLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 266, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(40, 40, 40))
+        );
+
+        PPainel.add(Adicionar_Alunos, "card2");
+
+        jLabel2.setText("Painel 2");
+
+        javax.swing.GroupLayout Ver_dadosLayout = new javax.swing.GroupLayout(Ver_dados);
+        Ver_dados.setLayout(Ver_dadosLayout);
+        Ver_dadosLayout.setHorizontalGroup(
+            Ver_dadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Ver_dadosLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2)
+                .addContainerGap(547, Short.MAX_VALUE))
+        );
+        Ver_dadosLayout.setVerticalGroup(
+            Ver_dadosLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(Ver_dadosLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2)
+                .addContainerGap(329, Short.MAX_VALUE))
+        );
+
+        PPainel.add(Ver_dados, "card3");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jButton1_Adicionar_Aluno)
-                .addContainerGap(697, Short.MAX_VALUE))
+            .addComponent(PPainel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(309, Short.MAX_VALUE)
-                .addComponent(jButton1_Adicionar_Aluno)
-                .addGap(19, 19, 19))
+            .addComponent(PPainel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1_Adicionar_AlunoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1_Adicionar_AlunoActionPerformed
-      System.out.println("Esse é um exemplo de ActionEvent");  // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1_Adicionar_AlunoActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+     CardLayout cl = (CardLayout) PPainel.getLayout();
+cl.show(PPainel, "card3");
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -97,6 +147,11 @@ public class InterfaceGrafica extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1_Adicionar_Aluno;
+    private javax.swing.JPanel Adicionar_Alunos;
+    private javax.swing.JPanel PPainel;
+    private javax.swing.JPanel Ver_dados;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
 }
